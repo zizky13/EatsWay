af297615e010bc490780e7fff5dc0130fa4343d7
zizky13 <zikar.nurizky@gmail.com>
2025-03-26 01:22:26 +0700

    Added filter page and toggle button (custom component)

---
+++ EatsWay/FilterView.swift
@@ -0,0 +1,25 @@
+//
+//  SwiftUIView.swift
+//  EatsWay
+//
+//  Created by Zikar Nurizky on 25/03/25.
+//
+
+import SwiftUI
+
+struct FilterView: View {
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text("Hello, world")
+            Color(.blue)
+            Color(.green)
+            Color(.yellow)
+        }
+        .padding(10)
+        .padding(.top, 30)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
